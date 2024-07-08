f6e3fa7cd251fac633822c52fbc36fa017a0cc4d
Pierre Ghueldre <ghueldre.pierre@laposte.net>
2024-07-08 14:49:42 +0200

    mobile ok

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ const workData = {
             "React Router": "./icons/react-router.svg",
             "Sass": "./icons/sass.svg"
         },
-        "detail_list": [
+        "task_list": [
             "Configuration de la navigation entre les pages de l'application avec React Router",
             "Développement de l'interface grâce à des composants React",
             "Utilisation des animations CSS",
@@ -23,6 +23,26 @@ const workData = {
         ],
         "link_list": {
             "Voir le code sur GitHub": "https://github.com/ghueldre-pierre/oc-projet-8" 
+        }
+    },
+    "nina": {
+        "url": "nina-carducci.fr",
+        "preview_image": "./images/nina_preview.jpg",
+        "intro": [
+            "Nina, photographe professionnelle à fait appel à mes services pour optimiser son site et améliorer sa visibilité sur le web."
+        ],
+        "role": "Développeur frontend",
+        "tech_list": {
+            "Google Lighthouse": "./icons/google_light_house.jpg"
+        },
+        "task_list": [
+            "Optimisation globale du site tant sur les performances que sur le SEO",
+            "Mise en place d'un référencement local en utilisant Schema.org",
+            "Paramètrage du site pour l'afficher sur les réseaux sociaux",
+            "Améliorations et tests d'accessibilité"
+        ],
+        "link_list": {
+            "Voir le code sur GitHub": "https://github.com/ghueldre-pierre/oc-projet-9" 
         }
     }
 };
@@ -57,7 +77,7 @@ const workContentDrivers = {
             element.append(li);
         }
     },
-    detail_list: (element, detailData) => {
+    task_list: (element, detailData) => {
         let li;
         for(const detailString of detailData) {
             li = document.createElement("li");
@@ -103,4 +123,33 @@ window.addEventListener("click", (event) => {
         workDialogElement.querySelector(".content").replaceChildren(dialogContent);
         workDialogElement.showModal();
     }
+});
+
+const workArticleTemplate = document.body.querySelector("#work_article_template");
+
+function createWorkArticle(workId) {
+    const workArticle = workArticleTemplate.content.cloneNode(true);
+    const data = workData[workId];
+    
+    let elementToProcess = workArticle.querySelector("article");
+    elementToProcess.dataset.workId = workId;
+
+    elementToProcess = workArticle.querySelector(".tab p");
+    elementToProcess.textContent = data.url;
+    
+    elementToProcess = workArticle.querySelector("img");
+    elementToProcess.setAttribute("src", data.preview_image)
+
+    return workArticle;
+}
+
+window.addEventListener("load", (event) => {
+    const workNames = Object.keys(workData);
+    const worksContainer = document.createDocumentFragment();
+    let newWork;
+    for(const id of workNames) {
+        newWork = createWorkArticle(id);
+        worksContainer.append(newWork);
+    }
+    document.body.querySelector(".works-grid").append(worksContainer);
 });
